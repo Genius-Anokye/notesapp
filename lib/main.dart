71e3493c559d54b2d3fba044e1a7d68d9fdc4e97
Genius-Anokye <124484460+Genius-Anokye@users.noreply.github.com>
2024-06-09 19:01:47 +0100

    step2

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: avoid_print
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:notesapp/views/login_view.dart';
 import 'package:notesapp/views/registerview.dart';
+import 'package:notesapp/views/verify_emailview.dart';
 
 import 'firebase_options.dart';
 
@@ -38,13 +40,17 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
-            // if (user?.emailVerified ?? false) {
-            //   return const Text("Done");
-            // } else {
-            //   return const VerifyEmailView();
-            // }
-            return const LoginView();
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print("Email is verified");
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text("Done");
           default:
             return const CircularProgressIndicator();
         }
@@ -52,5 +58,3 @@ class HomePage extends StatelessWidget {
     );
   }
 }
-
-
